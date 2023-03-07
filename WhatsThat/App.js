36ddf845f54cfdef69cd881f0526436bab85b320
Abdul Babar <arbabar786@gmail.com>
2023-03-07 17:25:16 +0000

    Added login and signup screens
    Added the login and signup screens inside the components folder, also imported them into app.js

--- WhatsThat/App.js
+++ WhatsThat/App.js
@@ -1,6 +1,9 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+import LoginScreen from './components/login';
+import SignUpScreen from './components/signup';
+
 export default function App() {
   return (
     <View style={styles.container}>
