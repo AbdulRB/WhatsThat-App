4d7ae8adacd661981ae6cd2e947201b787867b55
Abdul Babar <arbabar786@gmail.com>
2023-03-12 20:18:46 +0000

    Added contacts screen
    Added contacts.js which is the screen for managing contact endpoints

---
+++ WhatsThat/components/contacts.js
@@ -0,0 +1,73 @@
+import React, { Component } from 'react';
+import { View, Text, TextInput, TouchableOpacity, StyleSheet, Alert, FlatList} from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+export default class ContactsScreen extends Component {
+
+    constructor(props){
+        super(props);
+
+        this.state = {
+          isLoading: true,
+          listData: []
+        }
+    }
+
+    render(){
+
+      if (this.state.isLoading){
+        return (
+          <View style={styles.container}>
+              <Text>This is the contacts screen</Text>
+          </View>
+        )
+      }
+
+    }
+
+}
+
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      width: "80%",
+      alignItems: "stretch",
+      justifyContent: "center"
+    },
+    formContainer: {
+  
+    },
+    first_name:{
+      marginBottom: 5
+    },
+    last_name:{
+      marginBottom: 5
+    },
+    email:{
+      marginBottom: 5
+    },
+    password:{
+      marginBottom: 10
+    },
+    signUpBtn:{
+  
+    },
+    signup:{
+      justifyContent: "center",
+      textDecorationLine: "underline"
+    },
+    button: {
+      marginBottom: 30,
+      backgroundColor: '#2196F3'
+    },
+    buttonText: {
+      textAlign: 'center',
+      padding: 20,
+      color: 'white'
+    },
+    error: {
+        color: "red",
+        fontWeight: '900'
+    }
+  });
